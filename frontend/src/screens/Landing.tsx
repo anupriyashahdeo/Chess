9d55055e4f7352c7be497f31c3d54b33fb68f91e
anupriyashahdeo <anupriyashahdeo2001gmail.com>
2024-07-04 00:59:02 +0530

    v1 final version

--- frontend/src/screens/Landing.tsx
+++ frontend/src/screens/Landing.tsx
@@ -1,20 +1,29 @@
+import { useNavigate } from "react-router-dom"
+import { Button } from "../components/Button";
+
+
+
 export const Landing = () => {
-    return <div>
-        <div className="pt-8">
+    const navigate = useNavigate();
+    return <div className="flex justify-center">
+        <div className="pt-8 max-w-screen-lg">
             <div className="grid grid-cols-1 gap-4
             md:grid-cols-2">
                 <div className="flex justify-center">
                     <img src={"/chessboard.jpeg"}
                         className="max-w-96" />
                 </div >
-                <div>
-                    <h1 className="text-4xl font-bold">Play chess online on the  #2 site!</h1>
-                    <div className="mt-4">
-                        <button
-                            className="bg-blue-500 
-                    hover:bg-blue-700 text-white
-                    font-bold py-2  px-4 rounded">
-                            play online</button></div>
+                <div className="pt-16">
+                    <div className="flex justify-center">
+                    <h1 className="text-4xl text-white font-bold">Play chess online on the  #2 site!</h1>
+                    </div>
+                    <div className="mt-8 flex justify-center">
+                       <Button onClick={() =>{
+                        navigate("/game")
+                       }}>
+                          Play online
+                       </Button>
+                       </div>
                 </div>
             </div>
 
